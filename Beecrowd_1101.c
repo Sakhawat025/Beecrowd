5087c2d948a3adfd53a63e68ace4eca54f3871cb
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2023-10-25 21:43:09 +0600

    Add files via upload

---
+++ Beecrowd_1101.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j,t,sum=0,temp;
+    for(j = 0; j < 100; j++)
+    {
+        scanf("%d %d",&a,&b);
+        if(a <= 0 || b <= 0)
+            break;
+        if(a>b)
+        {
+            temp=a;
+            a=b;
+            b=temp;
+        }
+        for(i = a; i <= b; i++)
+        {
+            printf("%d ",i);
+            sum+=i;
+        }
+        printf("Sum=%d\n",sum);
+        sum=0;
+    }
+    return 0;
+}
